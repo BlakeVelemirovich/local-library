380dea0b3c9b2b3df73272da2a58ff958566eddd
DragunovVelimirovic <blake_v1993@hotmail.com>
2023-06-15 17:01:19 -0300

    added dotenv for secure dbconnection.

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const compression = require('compression');
+const helmet = require('helmet');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -11,11 +13,28 @@ const catalogRouter = require("./routes/catalog");
 
 const app = express();
 
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
 app.use(compression());
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+); 
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://blakevelemirovich:PrinceEmo@cluster0.cozje04.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB;
 
 main().catch((err) => console.log(err));
 async function main() {
